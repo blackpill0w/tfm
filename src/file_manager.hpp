8c1f00817ffb2049cff170bef66b4b0144eddcdd
blackpill0w <blackpill0w@protonmail.com>
2023-02-21 14:15:22 +0100

    Add File Manager's Layout

---
+++ src/file_manager.hpp
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <ncurses.h>
+
+#include "./ncurses_win.hpp"
+
+struct FileManager
+{
+public:
+   NcursesWin title_win{};
+   NcursesWin prev_win{};
+   NcursesWin cwd_win{};
+   NcursesWin content_win{};
+   NcursesWin cmd_win{};
+
+public:
+   FileManager(WINDOW *container)
+   {
+      resize(container);
+   }
+   void resize(WINDOW *container)
+   {
+      int maxy, maxx;
+      getmaxyx(container, maxy, maxx);
+
+      title_win.set(container, 1, maxx, 0, 0);
+      prev_win.set(container, maxy - 2, maxx / 3, 1, 0);
+      cwd_win.set(container, maxy - 2, maxx / 3, 1, maxx / 3);
+      content_win.set(container, maxy - 2, maxx / 3, 1, 2 * maxx / 3);
+      cmd_win.set(container, 1, maxx, maxy - 1, 0);
+   }
+   void refresh_fm()
+   {
+      title_win.refresh_win();
+      prev_win.refresh_win();
+      cwd_win.refresh_win();
+      content_win.refresh_win();
+      cmd_win.refresh_win();
+   }
+   void erase_fm()
+   {
+      title_win.erase_win();
+      prev_win.erase_win();
+      cwd_win.erase_win();
+      content_win.erase_win();
+      cmd_win.erase_win();
+   }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <locale>
 #include <ncurses.h>
 
+#include "./file_manager.hpp"
 #include "./ncurses_app.hpp"
 
 int main()
@@ -8,6 +9,7 @@ int main()
    setlocale(LC_ALL, "");
 
    NcursesApp app{};
+   FileManager fm{ stdscr };
 
    while (true)
    {
@@ -16,6 +18,31 @@ int main()
       {
          break;
       }
+      else if (input == KEY_RESIZE)
+      {
+         fm.resize(stdscr);
+      }
+      else if (input == 't')
+      {
+         wprintw(fm.title_win.getwin(), "sdkjf");
+      }
+      else if (input == 'p')
+      {
+         wprintw(fm.prev_win.getwin(), "sdkjf");
+      }
+      else if (input == 'c')
+      {
+         wprintw(fm.cwd_win.getwin(), "sdkjf");
+      }
+      else if (input == 'n')
+      {
+         wprintw(fm.content_win.getwin(), ": ");
+      }
+      else if (input == ':')
+      {
+         wprintw(fm.cmd_win.getwin(), ": ");
+      }
       app.refresh_app();
+      fm.refresh_fm();
    }
 }
